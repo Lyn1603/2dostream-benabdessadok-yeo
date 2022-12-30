35d42cda20496dfeb5e11a8d56b4cd1dc6c7b745
Sarayeo <yeokpeusseusarafiela@gmail.com>
2022-12-30 20:25:40 +0100

    :white_check_mark:

--- recherche.php
+++ recherche.php
@@ -9,7 +9,16 @@
     <label>Search</label>
     <input type="text" id="search" name="search">
 </form>
+<ul>
+    <button> Genre </button>
+    <button> Avis </button>
+    <button> Nom </button>
+    <button> Popularité </button>
+    <button> Vues </button>
+    <button> Classifications d'âges </button>
+</ul>
 <div id="showResults"></div>
+<div id="resultmovies"></div>
 </body>
 </html>
 <script src="./node_modules/axios/dist/axios.min.js"></script>
