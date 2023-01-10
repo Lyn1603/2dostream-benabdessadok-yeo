73a83b89a404a0d1e331ab1764b145d849ccaa17
Lynda Benabdessadok <lylyben74@gmail.com>
2023-01-10 08:07:31 +0100

    update bdd

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/dist/input.css" rel="stylesheet">
     <title>Connexion</title>
 </head>
 <body>
-<h1> Bonjour </h1>
+<h1 class="text-"> Bonjour </h1>
 <img src="" alt="logo 2doStream" >
 <h3> Connectez-vous </h3>
 <form method="post">
